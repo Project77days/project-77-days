def1d9fcc355ff382e6fb56d7cf65a13950a61d6
Ar3secchim <renarasecchim@gmail.com>
2023-12-13 20:29:09 -0300

    feat(config): config pwa

--- next.config.js
+++ next.config.js
@@ -1,17 +1,7 @@
 const withPWA = require("next-pwa");
-const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = withPWA({
-  pwa: {
-    dest: "public",
-    disable: !isProduction,
-  },
+  dest: "public",
   register: true,
   skipWaiting: true,
-});
-
-module.exports = withPWA({
-  // next.js config
-  reactStrictMode: false,
-  swcMinify: false,
 });
