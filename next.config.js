9a10f28804fae29788ce3aeb26f8d73c7d479f72
Renara Secchim <renarasecchim@gmail.com>
2023-12-13 19:38:49 +0000

    refactor(docs):update config pwa next

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withPWA = require('next-pwa')
+const isProduction = process.env.NODE_ENV === 'production'
+
+module.exports= withPWA({
+  pwa: {
+    dest: 'public',
+    disable:!isProduction,
+  },
+  register: true,
+  skipWaiting: true,
+})
+
+module.exports = withPWA({
+  // next.js config
+  reactStrictMode: false,
+  swcMinify: false,
+})
